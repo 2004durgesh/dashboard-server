1969399d25fd714939d97e06519918414d5c5712
Durgesh Kumar Dwivedi <durgeshdwivedi81@gmail.com>
2023-12-25 18:19:54 +0530

    Update MongoDB connection string

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`mongodb://localhost:27017/esamyak`)
+        const connectionInstance = await mongoose.connect(`mongodb+srv://durgeshdwivedi81:M6HZTFt821ugGmWK@cluster0.mrvh7lu.mongodb.net/esamyak`)
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("MONGODB connection FAILED ", error);
